fa2f0611024af09f77a50f62a3da19ed4f87d8f6
Austin Langley <austinwlangley@gmail.com>
2018-04-04 21:24:42 -0700

    first commit

---
+++ bisection.rb
@@ -0,0 +1,29 @@
+
+print "What number would you like to find the cube root of? "
+cube = gets.to_f
+print "How close do you want to get?"
+epsilon = gets.to_f
+
+num_guesses = 0
+low = 0
+high = cube
+
+guess = (high + low)/2.0
+
+while (guess**3 - cube).abs >= epsilon do
+  if guess**3 < cube
+    low = guess 
+  else
+    high = guess
+    end
+    guess = (high + low)/2.0
+    num_guesses += 1
+    puts "#{guess}"
+end
+
+puts "Number of guesses #{num_guesses}"
+puts "Estimating #{guess} for the cube root of #{cube}"
+puts "Due to epsilon of #{epsilon}"
+
+
+
