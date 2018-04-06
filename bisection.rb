be1e3c1f990f02e6f660c840d1f356c8997f988f
Austin Langley <austinwlangley@gmail.com>
2018-04-05 20:45:42 -0700

    accounted for taking cube roots of decimals

--- bisection.rb
+++ bisection.rb
@@ -5,25 +5,31 @@ print "How close do you want to get?"
 epsilon = gets.to_f
 
 num_guesses = 0
-low = 0
-high = cube
+
+if cube < 1
+  low = cube
+  high = cube * 2.0
+else
+  low = 0
+  high = cube  
+end
 
 guess = (high + low)/2.0
 
 while (guess**3 - cube).abs >= epsilon do
-  if guess**3 < cube
+  if guess**3 < cube  
     low = guess 
   else
     high = guess
     end
-    guess = (high + low)/2.0
-    num_guesses += 1
-    puts "#{guess}"
+  guess = (high + low)/2.0
+  num_guesses += 1
+  puts "#{guess}"
 end
 
-puts "Number of guesses #{num_guesses}"
-puts "Estimating #{guess} for the cube root of #{cube}"
-puts "Due to epsilon of #{epsilon}"
+puts "Number of guesses #{num_guesses}."
+puts "Estimating #{guess} for the cube root of #{cube}."
+puts "Due to epsilon of #{epsilon}."
 
 
 
